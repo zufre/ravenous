d18582e41bfecd7a2e802f940a43f7042b83ed41
Cédric David <cedric.david@btopenworld.com>
2018-12-19 15:47:31 +1000

    add favicon

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Cédric's Restaurant Finder</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={this.state.businesses} />
       </div>
